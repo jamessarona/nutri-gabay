cc7db8926f124b08fc6593c0d0e64b2786549873
James Angelo Sarona <jacsarona.official@gmail.com>
2023-12-14 22:17:42 +0800

    added screen for monitoring and evaluation

---
+++ lib/views/ui/answered_form_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class AnsweredFormScreen extends StatefulWidget {
+  final String appointmentId;
+  const AnsweredFormScreen({super.key, required this.appointmentId});
+
+  @override
+  State<AnsweredFormScreen> createState() => _AnsweredFormScreenState();
+}
+
+class _AnsweredFormScreenState extends State<AnsweredFormScreen> {
+  late Size screenSize;
+
+  @override
+  Widget build(BuildContext context) {
+    screenSize = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.grey[50],
+        appBar: AppBar(
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(0),
+            child: Container(
+                decoration: BoxDecoration(border: Border.all(width: 0.2))),
+          ),
+          toolbarHeight: 50,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.black),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          actions: [
+            Container(
+              margin: const EdgeInsets.only(right: 15),
+              padding: const EdgeInsets.fromLTRB(16, 45, 0, 0),
+              height: 40,
+              width: 150,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/logo-name.png"),
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ),
+          ],
+        ),
+        body: SizedBox(
+          height: double.infinity,
+          width: double.infinity,
+        ),
+      ),
+    );
+  }
+}
